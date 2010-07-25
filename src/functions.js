f5b040cdbaf82a1301d7923567abe4bea9b4a7d9
Michael Gundlach <michael@getadblock.com>
2010-07-25 21:58:14 +0000

    Update issue 2530 Add support for i18n_replacement_el
    svn path=/branches/issue-i18n_new/; revision=787

--- src/functions.js
+++ src/functions.js
@@ -42,20 +42,29 @@ function checkboxlabel_clicked() {
 }
 
 function translate(messageID, args) {
-  // If the user didn't specify a second parameter, then 'args' is
-  // set to undefined; and calling getMessage(messageID) is
-  // exactly equivalent to calling getMessage(messageID, undefined).
   return chrome.i18n.getMessage(messageID, args);
 }
+
 function localizePage() {
   //translate a page into the users language
-  $("[i18n]").each(function() {
-    $(this).text(translate($(this).attr("i18n")));
+  $("[i18n]:not(.i18n-replaced)").each(function() {
+    $(this).html(translate($(this).attr("i18n")));
   });
-  $("[i18n_value]").each(function() {
+  $("[i18n_value]:not(.i18n-replaced)").each(function() {
     $(this).val(translate($(this).attr("i18n_value")));
   });
-  $("[i18n_title]").each(function() {
+  $("[i18n_title]:not(.i18n-replaced)").each(function() {
     $(this).attr("title", translate($(this).attr("i18n_title")));
+  });
+  $("[i18n_replacement_el]:not(.i18n-replaced)").each(function() {
+    // Replace a dummy <a/> inside of localized text with a real element.
+    // Give the real element the same text as the dummy link.
+    var dummy_link = $("a", this);
+    var text = dummy_link.text();
+    var real_el = $("#" + $(this).attr("i18n_replacement_el"));
+    real_el.text(text).val(text).replaceAll(dummy_link);
+    // If localizePage is run again, don't let the [i18n] code above
+    // clobber our work
+    $(this).addClass("i18n-replaced");
   });
 }
