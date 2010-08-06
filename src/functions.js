83d14c08f8cfe54658deb1d08e1d106ac6f9f735
Famlam <fam.lam@live.nl>
2010-08-06 16:26:03 +0000

    Update issue 4188 Labels: ReadyForRelease I didn't get any errors visiting that domain, so I assume it is fixed...
    svn path=/branches/issue-4188/; revision=818

--- src/functions.js
+++ src/functions.js
@@ -18,7 +18,6 @@ time_log = function() { };
 // TODO: when they whitelist a page, make sure the top level domain is
 // whitelisted, even if they happened to be clicking inside an iframe.
 function page_is_whitelisted(whitelist, the_domain) {
-  if (the_domain == "health.google.com") return true;
   if (the_domain == "acid3.acidtests.org") return true;
   for (var i = 0; i < whitelist.length; i++) {
     if (the_domain.indexOf(whitelist[i]) != -1)
