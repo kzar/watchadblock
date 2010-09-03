9eb495cc6f068d8ca3814e54fce6558260caeba9
Michael Gundlach <michael@getadblock.com>
2010-09-03 20:47:55 +0000

    Update issue 572 Oops, make sure the wizards respond to Safari menu item clicks
    svn path=/branches/issue-572/; revision=982

--- src/adblock.js
+++ src/adblock.js
@@ -199,19 +199,19 @@ function adblock_begin_v2() {
 
     listen_for_broadcasts();
 
+    // The wizards by default don't respond to contextmenu clicks,
+    // because Chrome can't hide the contextmenus on whitelisted
+    // pages.  Now that we know we're not whitelisted, make the
+    // wizards respond.
+    may_open_blacklist_ui = true;
+    may_open_whitelist_ui = true;
+
     if (SAFARI) {
-      // Add entries to right click menu.
+      // Add entries to right click menu.  Unlike Chrome, we can make
+      // the menu items only appear on non-whitelisted pages.
       window.addEventListener("contextmenu", function(event) {
         safari.self.tab.setContextMenuEventUserInfo(event, true);
       }, false);
-    }
-    if (!SAFARI) {
-      // Make the right-click menu items do something (we can't actually
-      // hide them on tab selection change without 'tabs' permission,
-      // so we always show them but make them do nothing until we verify
-      // that we're not whitelisted.)
-      may_open_blacklist_ui = true;
-      may_open_whitelist_ui = true;
     }
 
     run_specials(data.features);
