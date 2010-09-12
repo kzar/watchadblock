9279072f76c740fb137ded2a928d5aef7d8d5099
Michael Gundlach <michael@getadblock.com>
2010-09-12 03:05:29 +0000

    typo
    svn path=/trunk/; revision=1052

--- src/thanks.py
+++ src/thanks.py
@@ -185,8 +185,8 @@ def main():
 
 
 
-    for i in range(5):
-        print()
+    for k in range(5):
+        print
     for d in thanked:
         print "$%.0f %s -- %s" % (d.amount, d.name, d.note)
     print
