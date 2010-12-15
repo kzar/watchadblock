7a5acd1f843ef7c580613d89233612f9f2f61b9a
Michael Gundlach <michael@getadblock.com>
2010-12-15 23:44:35 +0000

    Update issue 4865 Use bitwise ops for var unsupported Get rid of function old_style_to_new which is SelectorFilter._old_style_to_new but copy and modified... Use .test in place of .indexOf in one spot (thanks to Famlam for teaching me that that's faster :) ) Use [].join instead of string concatenation -- it's faster
    svn path=/branches/issue-4865/; revision=1495

--- src/filtering/filteroptimizer.js
+++ src/filtering/filteroptimizer.js
@@ -1,56 +1,16 @@
-//This file will clean up the filter lists when they are fetched.
-//It will remove broken filters, comments and unsupported things,
-//And it will already parse the rules on beforehand.
-
+// This function will clean up the filter lists when they are fetched.
+// It will remove broken filters, comments and unsupported things,
+// and it will already parse the rules on beforehand.
+// Input: text:string contents of a filter list
+// Returns: string contents of filter list after cleanup
 function cleanThisList(text) {
-  var unsupported = (ElementTypes.object_subrequest + ElementTypes.font +
-                     ElementTypes.dtd + ElementTypes.other + ElementTypes.xbl +
-                     ElementTypes.ping + ElementTypes.xmlhttprequest +
-                     ElementTypes.document + ElementTypes.elemhide)
+  var unsupported = (ElementTypes.object_subrequest | ElementTypes.font |
+                     ElementTypes.dtd | ElementTypes.other | ElementTypes.xbl |
+                     ElementTypes.ping | ElementTypes.xmlhttprequest |
+                     ElementTypes.document | ElementTypes.elemhide);
   var ignoringCount = 0;
-  // Convert a deprecated "old-style" filter text to the new style.
-  function old_style_to_new(text) {
-    // Old-style is domain#node(attr=value) or domain#node(attr)
-    // domain and node are optional, and there can be many () parts.
-    text = text.replace('#', '##');
-    var parts = text.split('##'); // -> [domain, rule]
-    var domain = parts[0];
-    var rule = parts[1];
-    // Make sure the rule has only the following two things:
-    // 1. a node -- this is optional and must be '*' or alphanumeric
-    // 2. a series of ()-delimited arbitrary strings -- also optional
-    //    the ()s can't be empty, and can't start with '='
-    if (rule.length == 0 || 
-        !/^(?:\*|[a-z0-9]*)(?:\([^=][^\)]*\))*/i.test(rule))
-      return false;
-
-    var first_segment = rule.indexOf('(');
-
-    if (first_segment == -1)
-      return domain + '##' + rule;
-
-    var node = rule.substring(0, first_segment);
-    var segments = rule.substring(first_segment);
-
-    // turn all (foo) groups into [foo]
-    segments = segments.replace(/\((.*?)\)/g, "[$1]");
-    // turn all [foo=bar baz] groups into [foo="bar baz"]
-    // Specifically match:    = then not " then anything till ]
-    segments = segments.replace(/=([^"][^\]]*)/g, '="$1"');
-    // turn all [foo] into .foo, #foo
-    // #div(adblock) means all divs with class or id adblock
-    // class must be a single class, not multiple (not #*(ad listitem))
-    // I haven't ever seen filters like #div(foo)(anotherfoo), so ignore these
-    var resultFilter = node + segments;
-    var match = resultFilter.match(/\[([^\=]*?)\]/);
-    if (match)
-      resultFilter = resultFilter.replace(match[0], "#" + match[1]) +
-       "," + resultFilter.replace(match[0], "." + match[1]);
-
-    return domain + "##" + resultFilter;
-  }
-
   //Really change the filter
+  //Input: filter:string one line from a filter list subscription
   //Returns the filter if the filter was correct, 
   //'false' if the filter was broken
   //'' if the filter was ignored
@@ -65,12 +25,14 @@ function cleanThisList(text) {
       return '';
 
     //If it is a hiding rule...
-    if (filter.indexOf('##') > -1 || /#.*\(/.test(filter)) {
+    if (/##/.test(filter) || /#.*\(/.test(filter)) {
       //...convert old-style filters to new ones...
       if (filter.indexOf('##') == -1) {
-        filter = old_style_to_new(filter);
-        if (!filter)
+        try {
+          filter = SelectorFilter._old_style_to_new(filter);
+        } catch (e) { // couldn't parse it.
           return false;
+        }
       }
       //...check if the domain is of a valid type...
       var parts = filter.split('##');
@@ -125,15 +87,15 @@ function cleanThisList(text) {
 
   var lines = text.split('\n');
   delete text;
-  var result = '';
+  var result = [];
   for (var i=0; i<lines.length; i++) {
     var newfilter = optimizeFilter(lines[i]);
     if (newfilter)
-      result = result + newfilter + '\n';
+      result.append(newfilter);
     else if (newfilter === false)
       log("Filter '" + lines[i] + "' could not be parsed");
   }
   if (ignoringCount)
     log('Ingoring ' + ignoringCount + ' unsupported rule(s)');
-  return result;
+  return result.join('\n') + '\n';
 }
