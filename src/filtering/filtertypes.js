0de623fdbd90bace157e055ebd1c869a1aa79b85
Michael Gundlach <michael@getadblock.com>
2011-01-14 23:10:12 +0000

    Typo
    svn path=/branches/issue-3873/; revision=1650

--- src/filtering/filtertypes.js
+++ src/filtering/filtertypes.js
@@ -23,7 +23,7 @@ Filter.fromText = function(text) {
 
     if (Filter.isSelectorFilter(text))
       cache[text] = new SelectorFilter(text);
-    else if Filter.isWhitelistFilter(text)
+    else if (Filter.isWhitelistFilter(text))
       cache[text] = new WhitelistFilter(text);
     else
       cache[text] = new PatternFilter(text);
