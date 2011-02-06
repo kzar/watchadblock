932ae90acda933433ad11cbe77a54b31633e6a86
Michael Gundlach <michael@getadblock.com>
2011-02-06 22:46:05 +0000

    Update issue 4571 Work on merging browser button Fix bug in functions.js that required filteroptions.js to be included Install page shows red icon, not gray icon Donation link in button menu is now just a plain passthru
    svn path=/branches/issue-4571/; revision=1730

--- src/button/temporary_background_code.js
+++ src/button/temporary_background_code.js
@@ -1,62 +1,12 @@
-// This code exists to make the Browser Button extension work.  Once we don't need the
-// browser button extension, we should be able to integrate by
-// deleting this file
-// deleting button_extension_id from functions.js
-// deleting reference to this file from background.html
-// adding the browser_action back into manifest.json
-// removing the options/general reference to installing the second extension
-// Changing the donation request in options/index
-
-
-if (!SAFARI) {
-  chrome.extension.onRequestExternal.addListener(function(request, sender, sendResponse) {
-    if (sender.id != button_extension_id)
-      return;
-    var result = utils[request.fn](request.options, sender);
-    sendResponse(result);
-  });
-
-  utils.pause_adblock = function(options) {
-    localStorage.setItem('adblock_is_paused', true);
-    updateButtonUIAndContextMenus();
-  }
-
-  utils.unpause_adblock = function(options) {
-    localStorage.removeItem('adblock_is_paused');
-    updateButtonUIAndContextMenus();
-  }
-  
-  utils.get_whitelist = function(options) {
-    var custom_filters = utils.get_custom_filters_text({}).split('\n');
-    // Down-convert custom filters into old whitelist entries
-    var whitelist = custom_filters.map(function(text) {
-      var dot_match = text.match(/^@@\|\|\*\.([a-zA-Z0-9-.]+)\^\$document$/);
-      if (dot_match)
-        return '.' + dot_match[1];
-      var domain_match = text.match(/^@@\|\|([a-zA-Z0-9-.]+)\^\$document$/);
-      if (domain_match)
-        return domain_match[1];
-      else
-        return null; // Not a whitelist entry
-    });
-    // Remove non-whitelist entries
-    return whitelist.filter(function(text) { return text != null; });
-  }
-
-  utils.add_to_whitelist = function(options) {
-    utils.add_custom_filter({filter: '@@||' + options.domain + '^$document'});
-  }
-
-  utils.remove_from_whitelist = function(options) {
-    utils.try_to_unwhitelist({url:"http://" + options.domain});
-  }
-
-
-  // Since we've pulled out the browser action temporarily, make API calls noops
-  // instead of crashing.
-  chrome.browserAction = {
-    setPopup: function() {},
-    setTitle: function() {},
-    setIcon: function() {},
-  };
-}
+// To finish converting to a merge button:
+//
+// delete this file
+// ship browser button for adblock update that kills the icon if no response from adblock (search history, it should be in svn)
+//   or can we uninstall it for them, or disable it for them?
+// i18n on button
+// one-time message in button for existing users explaining what's going on
+//   detect browser button for adblock.  exists?
+//     yes: tell them to right click and Uninstall the other one.
+//     no : tell them "AdBlock now has a built-in toolbar button, hideable by right clicking and choosing "Hide Button"." or something.
+// remove the new release temp saw_badge_version thing in background
+// display the badge for this button release
