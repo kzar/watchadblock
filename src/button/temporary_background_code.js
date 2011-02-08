5a48395af16063ab2bc0d43b5f26bada4a942dcd
Michael Gundlach <michael@getadblock.com>
2011-02-08 16:51:38 +0000

    Update issue 4571 New Version notification with custom message for Browser Button For AdBlock users
    svn path=/branches/issue-4571/; revision=1732

--- src/button/temporary_background_code.js
+++ src/button/temporary_background_code.js
@@ -3,8 +3,3 @@
 // delete this file
 // ship browser button 0.0.13
 // i18n on button
-// one-time message in button for existing users explaining what's going on
-//   detect browser button for adblock.  exists?
-//     yes: "This toolbar button is now built into AdBlock!  Kindly go to chrome://extensions and click 'Browser Button For AdBlock | Uninstall'."
-//     no : "This toolbar button :)  Click 'Hide Button' below to hide it."
-// display the badge for this button release (2.3)
