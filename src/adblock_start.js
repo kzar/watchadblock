1667e3c8f45431af460dec755222252ca04afc8c
Famlam <fam.lam@live.nl>
2011-03-28 15:37:56 +0000

    Update issue 5405 + Oops
    svn path=/branches/issue-5405/; revision=2124

--- src/adblock_start.js
+++ src/adblock_start.js
@@ -143,9 +143,9 @@ function adblock_begin() {
       return;
     }
 
-    if (data.selectors && data.selectors.length != 0) {
+    if (data.selectors && data.selectors.length != 0)
       block_list_via_css(data.selectors);
-}
+
     //Chrome can't block resources immediately. Therefore all resources
     //are cached first. Once the filters are loaded, simply remove them
     if (!SAFARI) {
