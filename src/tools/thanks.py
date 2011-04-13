a0bf8cae330e249324680e94ab6d3b93fccefa79
Michael Gundlach <michael@getadblock.com>
2011-04-13 22:15:56 +0000

    Working to switch away from personal email address for work
    svn path=/trunk/; revision=2158

--- src/tools/thanks.py
+++ src/tools/thanks.py
@@ -125,19 +125,23 @@ I wrote AdBlock in the hope that I could make people's lives better.  Your
 donation tells me that I did it :)  Thank you very, very much!  You are so
 great!
 
-It has been scary taking a risk, quitting my job, and hoping to support my
-family and fund AdBlock development using only AdBlock donations.  Not a lot
-of users donate yet, which makes your donation even more appreciated --
-your donation is way above what most users give: zero.  Did I say thank you
-yet?  Thank you! :D
+It's been a little scary in the few months since I quit my job, hoping that my
+users would donate enough that I could support my family and fund more AdBlock
+development.  Not many users donate yet, which makes your donation even more
+appreciated -- your donation is way above what most users give: zero.
+
+In any case, Katie and I have decided that it's important enough work that I
+should keep doing it whether we can live off of donations or not, until we
+start running out of savings.  Your donation just tipped the scales a little
+further away from going broke... did I say thank you yet?  Thank you!  :D  
 
 Happy ad blocking,
 - Michael
 
 PS: If you don't mind, would you go to http://chromeadblock.com/donate/thanks/
 and help me spread the word?  I tried setting it up so PayPal would show you
-that automatically after you donated, but I couldn't figure it out!  Anyway, it
-would help me IMMENSELY :)
+that automatically after you donated, but it doesn't seem to work reliably.
+Anyway, it would help me IMMENSELY :)
 
 
 %(original)s
@@ -150,7 +154,7 @@ def send(from_, to, subject, body):
     server.ehlo()
     server.starttls()
     server.ehlo()
-    server.login('gundlach@gmail.com', GLOBAL_password)
+    server.login('adblockforchrome@gmail.com', GLOBAL_afc_password)
     msg = '''\
 From: %s
 To: %s
@@ -209,7 +213,7 @@ def with_corrected_nicknames(donations):
     f.close()
     print "Press enter to edit nicknames."
     raw_input()
-    os.system('vim /tmp/nicknames.csv')
+    os.system('vim -c "set nowrap" -c "set tabstop=20" /tmp/nicknames.csv')
     reader = csv.reader(open('/tmp/nicknames.csv'), delimiter='\t')
     return [ Donation(amount=float(amount), browser=browser, msgid=msgid,
                       email=email, name=name, nickname=nickname, note=note)
@@ -242,6 +246,8 @@ def main():
         return
     global GLOBAL_password
     GLOBAL_password = getpass.getpass("Password for gundlach@gmail.com: ")
+    global GLOBAL_afc_password
+    GLOBAL_afc_password = getpass.getpass("Password for adblockforchrome@gmail.com: ")
     if sys.argv[1] == 'yes':
         thank_notes()
     else:
