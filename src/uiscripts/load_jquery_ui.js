1529896b6894763ef4a9a1bd517bb11507b3cdee
Famlam <fam.lam@live.nl>
2011-06-26 23:05:09 +0000

    Update issue 5739 Status:ReadyForRelease Launching the nuke for readfile...
    svn path=/branches/issue-5739/; revision=2584

--- src/uiscripts/load_jquery_ui.js
+++ src/uiscripts/load_jquery_ui.js
@@ -11,24 +11,25 @@ function load_jquery_ui(callback) {
       attr('href', url);
     $("head").append(link);
   }
-  BGcall('readfile', "jquery/jquery-ui.custom.min.js", function(result) {
-    eval(result); // suck it, Trebek
+  $.ajax({
+    url: chrome.extension.getURL("jquery/jquery-ui.custom.min.js"),
+    success: function(result) {
+      eval(result); // suck it, Trebek
 
-    load_css("jquery/css/custom-theme/jquery-ui-1.8.custom.css");
-    load_css("jquery/css/override-page.css");
+      load_css("jquery/css/custom-theme/jquery-ui-1.8.custom.css");
+      load_css("jquery/css/override-page.css");
 
-    if (SAFARI) {
-      // chrome.i18n.getMessage() lazily loads a file from disk using xhr,
-      // but the page itself doesn't have access to extension resources.
-      // Since we'll be using getMessage(), we have to ask the background
-      // page for the data.
-      BGcall('get_l10n_data', function(data) {
-        chrome.i18n._setL10nData(data);
+      if (SAFARI) {
+        // chrome.i18n.getMessage() lazily loads a file from disk using xhr,
+        // but the page itself doesn't have access to extension resources.
+        // Since we'll be using getMessage(), we have to ask the background
+        // page for the data.
+        BGcall('get_l10n_data', function(data) {
+          chrome.i18n._setL10nData(data);
+          callback();
+        });
+      } else
         callback();
-      });
-    }
-    else {
-      callback();
     }
   });
 }
