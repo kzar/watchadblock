816feb26fb9b9dd6c32f8a45e68b0e9c1a7bcea4
adblockforchrome@gmail.com <adblockforchrome@gmail.com>
2011-09-07 18:01:46 +0000

    Update issue 5959 Safari-only
    svn path=/branches/issue-5959/; revision=2961

--- src/filtering/filternormalizer.js
+++ src/filtering/filternormalizer.js
@@ -64,8 +64,9 @@ var FilterNormalizer = {
       if ($(parts[1] + ',html').length == 0)
         throw "Caused other selector filters to fail";
 
-      // On a few sites, we have to ignore [style] rules due to crbug 68705
-      if (/style([\^\$\*]?=|\])/.test(filter)) {
+      // On a few sites, we have to ignore [style] rules due to crbug 68705.
+      // Doesn't happen in Safari.
+      if (!SAFARI && /style([\^\$\*]?=|\])/.test(filter)) {
         var ignoreStyleRulesOnTheseSites = "~mail.google.com,~mail.yahoo.com";
         if (filter[0] != "#") ignoreStyleRulesOnTheseSites += ",";
         filter = ignoreStyleRulesOnTheseSites + filter;
