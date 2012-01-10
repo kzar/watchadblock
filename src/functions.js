7b05ab5bc40829efc6787491a31e61bef2b7cf79
adblockforchrome@gmail.com <adblockforchrome@gmail.com>
2012-01-10 15:53:17 +0000

    Update issue 6330 Status: Fixed Merged into trunk
    svn path=/trunk/; revision=3428

--- src/functions.js
+++ src/functions.js
@@ -45,21 +45,30 @@ function localizePage() {
   });
 }
 
-// Return the scheme and domain for the given url.
-// TODO I have code in a few places that extracts domains, I think.
-// Deduplicate it.
-url_parts = function(url) {
-  var parts = url.match("(.*?)://(..*?)/");
-  if (!parts) // may be "about:blank" or similar
-    parts = url.match("(.*?):(.*)");
-  // TODO: crbug.com/81298 sometimes makes this break, but it doesn't
-  // impact users visibly so I'm letting it break.
-  var scheme = parts[1];
-  var domain = parts[2];
-  return {
-    scheme: scheme,
-    domain: domain
-  };
+// Parse a URL. Based upon http://blog.stevenlevithan.com/archives/parseuri
+// parseUri 1.2.2, (c) Steven Levithan <stevenlevithan.com>, MIT License
+// Inputs: url: the URL you want to parse
+// Outputs: object containing all parts of |url| as attributes
+parseUri = function(url) {
+  var matches = /^(([^:]+(?::|$))(?:(?:[^:]+:)?\/\/)?(?:[^:@]*(?::[^:@]*)?@)?(([^:\/?#]*)(?::(\d*))?))((?:[^?#\/]*\/)*[^?#]*)(\?[^#]*)?(\#.*)?/.exec(url);
+  // The key values are identical to the JS location object values for that key
+  var keys = ["href", "origin", "protocol", "host", "hostname", "port",
+              "pathname", "search", "hash"];
+  var uri = {};
+  for (var i=0; i<keys.length; i++)
+    uri[keys[i]] = matches[i] || "";
+  return uri;
+};
+// Parses the search part of a URL into an key: value object.
+// e.g., ?hello=world&ext=adblock would become {hello:"world", ext:"adblock"}
+// Inputs: search: the search query of a URL. Must have &-separated values.
+parseUri.parseSearch = function(search) {
+  // Fails if a key exists twice (e.g., ?a=foo&a=bar would return {a:"bar"}
+  var queryKeys = {};
+  search.replace(/(?:^\?|&)([^&=]*)=?([^&]*)/g, function () {
+    if (arguments[1]) queryKeys[arguments[1]] = arguments[2];
+  });
+  return queryKeys;
 }
 
 // TODO: move back into background.html since Safari can't use this
--- src/uiscripts/top_open_whitelist_ui.js
+++ src/uiscripts/top_open_whitelist_ui.js
@@ -10,12 +10,11 @@ function top_open_whitelist_ui() {
 
   // Safari's document.location breaks in the feed reader if the feed is fetched via https. Normal
   // feeds have URLs with scheme replaced with "feed", but HTTPS feeds have the scheme replaced with
-  // "feed:https", which isn't exatly a valid scheme. Safari's document.location can't handle that,
+  // "feed:https", which isn't exactly a valid scheme. Safari's document.location can't handle that,
   // so its domain property will be empty; fortunately, it puts a proper https url into the pathname
   // property and we can use it.
   if (SAFARI && domain === "" && document.location.href.indexOf("feed:https") === 0) {
-    var parts = url_parts(document.location.pathname);
-    domain = parts.domain;
+    domain = parseUri(document.location.pathname).hostname;
   }
 
   // defined in blacklister.js
