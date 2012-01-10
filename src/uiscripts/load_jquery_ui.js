aeaf5819cec01be6c62bc8813c75e2f02b1ab299
adblockforchrome@gmail.com <adblockforchrome@gmail.com>
2012-01-10 18:25:57 +0000

    Update issue 6360 Do as suggested in comment 25.
    Tested in Chrome 17, Chrome 18, and Safari.  All 3 work and
    the blacklist and whitelist wizards show up correctly.
    
    Famlam, please review this, but I won't set to NeedsReview
    because comment 23 needs to be addressed :) 
    
    svn path=/branches/issue-6360/; revision=3431

--- src/uiscripts/load_jquery_ui.js
+++ src/uiscripts/load_jquery_ui.js
@@ -11,29 +11,27 @@ function load_jquery_ui(callback) {
       attr('href', url);
     $("head").append(link);
   }
-  BGcall('readfile', "jquery/jquery.min.js", function(result) {
-    eval(result); // suck it, Trebek
+  load_css("jquery/css/custom-theme/jquery-ui-1.8.custom.css");
+  load_css("jquery/css/override-page.css");
+
+  if (!SAFARI) {
+    // Chrome already loaded jQueryUI via executeScript
+    callback();
+  }
+  else {
     BGcall('readfile', "jquery/jquery-ui.custom.min.js", function(result) {
-      eval(result); // suck it again, Trebek
+      eval(result); // suck it, Trebek
 
-      load_css("jquery/css/custom-theme/jquery-ui-1.8.custom.css");
-      load_css("jquery/css/override-page.css");
-
-      if (SAFARI) {
-        // chrome.i18n.getMessage() lazily loads a file from disk using xhr,
-        // but the page itself doesn't have access to extension resources.
-        // Since we'll be using getMessage(), we have to ask the background
-        // page for the data.
-        BGcall('get_l10n_data', function(data) {
-          chrome.i18n._setL10nData(data);
-          callback();
-        });
-      }
-      else {
+      // chrome.i18n.getMessage() lazily loads a file from disk using xhr,
+      // but the page itself doesn't have access to extension resources.
+      // Since we'll be using getMessage(), we have to ask the background
+      // page for the data.
+      BGcall('get_l10n_data', function(data) {
+        chrome.i18n._setL10nData(data);
         callback();
-      }
+      });
     });
-  });
+  }
 }
 
 //@ sourceURL=/uiscripts/load_jquery_ui.js
