d7b1f2f83985b32363f5914b91e10a81310b5ae0
adblockforchrome@gmail.com <adblockforchrome@gmail.com>
2012-01-13 19:30:59 +0000

    Update issue 6170 Work on popup.  - Wrapped all .js in a single $(function(){}).  - Moved all inline styles into .css file.  - Generate entire iframe the moment it's requested, rather than having    an empty iframe sits around, which makes CSP generate an error about    loading about:blank.
    Unfortunately, I'm still getting 5 style CSP errors which I bet are due
    to jQuery.
    
    svn path=/branches/issue-6170/; revision=3492

--- src/button/popup.js
+++ src/button/popup.js
@@ -4,8 +4,8 @@
 
 var BG = chrome.extension.getBackgroundPage();
 
-// Set menu entries appropriately for the selected tab.
-function customize_for_this_tab() {
+$(function() {
+  // Set menu entries appropriately for the selected tab.
   $(".menu-entry, .separator").hide();
 
   BG.getCurrentTabInfo(function(info) {
@@ -35,25 +35,18 @@ function customize_for_this_tab() {
     for (var div in shown)
       if (shown[div]) 
         $('#' + div).show();
-      });
-}
-
-$(function() {
-  customize_for_this_tab();
-});
+  });
 
 
+  $("#titletext span").click(function() {
+    var url = "https://chrome.google.com/webstore/detail/gighmmpiobklfepjocnamgkkbiglidom";
+    BG.openTab(url);
+  });
 
-$("#titletext span").click(function() {
-  var url = "https://chrome.google.com/webstore/detail/gighmmpiobklfepjocnamgkkbiglidom";
-  BG.openTab(url);
-});
-
-$("#newtitle").text(translate("newtitle", ["2.3.0"]));
+  $("#newtitle").text(translate("newtitle", ["2.3.0"]));
 
 
 
-$(function() {
   var info_ver = chrome.extension.getBackgroundPage().version_to_notify;
   // If there was badge text set, informing the user of a new
   // version, clear it.
@@ -70,121 +63,116 @@ $(function() {
       storage_set('saw_badge_info_version', info_ver);
     });
   }
-});
 
 
 
-
-$("#div_status_whitelisted a").click(function() {
-  BG.getCurrentTabInfo(function(info) {
-    if (BG.try_to_unwhitelist(info.tab.url)) {
-      // Reload the tab
-      chrome.tabs.update(info.tab.id, {url: info.tab.url});
-      window.close();
-    } else {
-      $("#div_status_whitelisted").
-      replaceWith(translate("disabled_by_filter_lists"));
-    }
+  $("#div_status_whitelisted a").click(function() {
+    BG.getCurrentTabInfo(function(info) {
+      if (BG.try_to_unwhitelist(info.tab.url)) {
+        // Reload the tab
+        chrome.tabs.update(info.tab.id, {url: info.tab.url});
+        window.close();
+      } else {
+        $("#div_status_whitelisted").
+        replaceWith(translate("disabled_by_filter_lists"));
+      }
+    });
   });
-});
 
 
 
-$(function() {
   $("#div_status_paused a").click(function() {
     BG.sessionStorage.removeItem('adblock_is_paused');
     BG.handlerBehaviorChanged();
     BG.updateButtonUIAndContextMenus();
     window.close();
   });
-});
 
 
 
-$("#div_pause_adblock").click(function() {
-  BG.sessionStorage.setItem('adblock_is_paused', true);
-  BG.updateButtonUIAndContextMenus();
-  chrome.contextMenus.removeAll();
-  window.close();
-});
-
-
-
-
-$("#div_blacklist").click(function() {
-  BG.getCurrentTabInfo(function(info) {
-    BG.emit_page_broadcast(
-      {fn:'top_open_blacklist_ui', options: { nothing_clicked: true }},
-      { tab: info.tab } // fake sender to determine target page
-    );
+  $("#div_pause_adblock").click(function() {
+    BG.sessionStorage.setItem('adblock_is_paused', true);
+    BG.updateButtonUIAndContextMenus();
+    chrome.contextMenus.removeAll();
     window.close();
   });
-});
 
-$("#div_whitelist_page").click(function() {
-  BG.getCurrentTabInfo(function(info) {
-    BG.create_page_whitelist_filter(info.tab.url);
-    // Reload the tab
-    chrome.tabs.update(info.tab.id, {url: info.tab.url});
-    window.close();
+
+
+
+  $("#div_blacklist").click(function() {
+    BG.getCurrentTabInfo(function(info) {
+      BG.emit_page_broadcast(
+        {fn:'top_open_blacklist_ui', options: { nothing_clicked: true }},
+        { tab: info.tab } // fake sender to determine target page
+      );
+      window.close();
+    });
   });
-});
 
-
-
-$("#div_whitelist").click(function() {
-  BG.getCurrentTabInfo(function(info) {
-    BG.emit_page_broadcast(
-      {fn:'top_open_whitelist_ui', options:{}},
-      { tab: info.tab } // fake sender to determine target page
-    );
-    window.close();
+  $("#div_whitelist_page").click(function() {
+    BG.getCurrentTabInfo(function(info) {
+      BG.create_page_whitelist_filter(info.tab.url);
+      // Reload the tab
+      chrome.tabs.update(info.tab.id, {url: info.tab.url});
+      window.close();
+    });
   });
-});
 
 
 
-$(function() {
+  $("#div_whitelist").click(function() {
+    BG.getCurrentTabInfo(function(info) {
+      BG.emit_page_broadcast(
+        {fn:'top_open_whitelist_ui', options:{}},
+        { tab: info.tab } // fake sender to determine target page
+      );
+      window.close();
+    });
+  });
+
+
+
   $("#div_show_resourcelist").click(function() {
     BG.getCurrentTabInfo(function(info) {
       BG.launch_resourceblocker(info.tab.id);
     });
   });
-});
 
 
-
-$(function() {
   $("#div_report_an_ad").click(function() {
     BG.getCurrentTabInfo(function(info) {
       var url = "pages/adreport.html?url=" + escape(info.tab.url);
       BG.openTab(url, true);
     });
   });
-});
 
 
 
-$("#div_options").click(function() {
-  BG.openTab("options/index.html");
-});
+  $("#div_options").click(function() {
+    BG.openTab("options/index.html");
+  });
 
 
 
-$("#div_help_hide").click(function() {
-  $("#help_hide_explanation").slideDown();
-});
+  $("#div_help_hide").click(function() {
+    $("#help_hide_explanation").slideDown();
+  });
 
 
 
-$(function() {
   var state = "initial";
   var bodysize = { width: $("body").width(), height: $("body").height() };
   var userId = storage_get("userid");
   var payHref = "http://chromeadblock.com/pay/?source=P&small=true&u=" + userId;
   $("#pay_open").click(function() {
     if (state == "initial") {
-      $("#payment_wrapper iframe").attr("src", payHref);
+      $("<iframe>", {
+        width: "100%",
+        height: "100%",
+        frameBorder: 0,
+        src: payHref
+      }).appendTo("#payment_wrapper");
     }
     if (state == "open")
       return;
@@ -193,8 +181,8 @@ $(function() {
     $("body").animate({width: 780, height: 490}, {queue:false});
     $("#menu-items").slideUp();
     $("#payment_wrapper").
-    width(0).height(0).show().
-    animate({width: 730, height: 450}, {queue:false});
+      width(0).height(0).show().
+      animate({width: 730, height: 450}, {queue:false});
   });
   $("#pay_close").click(function() {
     if (state != "open")
@@ -206,9 +194,7 @@ $(function() {
     $("#pay_close").hide();
     $("#payment_wrapper").slideUp();
   });
-});
 
 
-$(function() {
   localizePage();
 });
