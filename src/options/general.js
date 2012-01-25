9cf35f7a3cf58656231f1cee9fa054e70ed93565
adblockforchrome@gmail.com <adblockforchrome@gmail.com>
2012-01-25 17:37:36 +0000

    Move options JS into their own files. No modifications of the JS yet.
    Options is broken now, because index.html loads general.html
    via ajax and injects it into the page; the injected html
    then tries to insert a script tag which is ignored.
    
    
    svn path=/branches/issue-6425/; revision=3515

---
+++ src/options/general.js
@@ -0,0 +1,58 @@
+    $(function() {
+      // Check or uncheck each loaded DOM option checkbox according to the 
+      // user's saved settings.
+      function load_options() {
+        // Check or uncheck each option.
+        for (var name in optionalSettings) {
+          $("#enable_" + name).
+            attr("checked", optionalSettings[name]);
+        }
+        $("input.feature:checkbox").change(function() {
+          var is_enabled = $(this).is(':checked');
+          var name = this.id.substring(7); // TODO: hack
+          BGcall("set_setting", name, is_enabled);
+        });
+      }
+      load_options();
+    });
+
+
+
+      // TODO: This is a dumb race condition, and still has a bug where
+      // if the user reloads/closes the options page within a second
+      // of clicking this, the filters aren't rebuilt.  Call this inside
+      // the feature change handler if it's this checkbox being clicked.
+      $("#enable_show_google_search_text_ads").change(function() {
+        // Give the setting a sec to get saved by the other
+        // change handler before recalculating filters.
+        window.setTimeout(function() { 
+          BGcall("update_filters");
+        }, 1000);
+      });
+
+
+
+
+      $(function() {
+        $("#webrequest_warning").toggle($("#enable_use_webrequest_blocking").is(":checked"));
+        $("#enable_use_webrequest_blocking").click(function() {
+          $("#webrequest_warning").toggle(this.checked);
+        });
+      });
+
+
+
+
+        $("#enable_show_advanced_options").change(function() {
+          // Reload the page to show or hide the advanced options on the
+          // options page -- after a moment so we have time to save the option.
+          // Also, disable all advanced options, so that non-advanced users will
+          // not end up with debug/beta/test options enabled.
+          if (!this.checked)
+            $(".advanced :checkbox:checked").each(function() {
+              BGcall("set_setting", this.id.substr(7), false);
+            });
+          window.setTimeout(function() {
+            window.location.reload();
+          }, 50);
+        });
---
+++ src/options/index.js
@@ -0,0 +1,69 @@
+      function load_options() {
+        // Check or uncheck each option.
+        BGcall("get_settings", function(settings) {
+          optionalSettings = settings;
+          $("#tabpages").
+            tabs({ 
+              spinner: "",
+              cache: true,
+              cookie: {},
+              load: function() {
+                //translation
+                localizePage();
+
+                $(".advanced").toggle(optionalSettings.show_advanced_options);
+                $(".chrome-only").toggle(!SAFARI);
+              },
+            }).
+            show();
+        });
+      }
+      var optionalSettings = {};
+      load_options();
+
+      function displayVersionNumber() {
+        try {
+          var xhr = new XMLHttpRequest();
+          xhr.open("GET", chrome.extension.getURL('manifest.json'), true);
+          xhr.onreadystatechange = function() {
+            if(this.readyState == 4) {
+              var theManifest = JSON.parse(this.responseText);
+              $("#version_number").text(translate("optionsversion", [theManifest.version]));
+            }
+          };
+          xhr.send();
+        } catch (ex) {} // silently fail
+      }
+
+      $(function() {
+        displayVersionNumber();
+
+        if (navigator.language.substring(0, 2) != "en")
+          $("#translation_credits").text(translate("translator_credit"));
+
+        //translation
+        localizePage();
+      });
+
+
+
+
+        $(function() {
+          if (SAFARI && LEGACY_SAFARI) {
+            $("#safari50_updatenotice").show();
+          }
+
+          $('#paymentlink').click(function() {
+            BGcall("storage_get", "userid", function(userId) {
+              var href = "http://chromeadblock.com/pay/?source=O&u=" + userId;
+              if (SAFARI) {
+                // Safari target=_blank opens a new window by default, so we have to force its
+                // height to be correct.
+                window.open(href, "payment", 'location=0,status=0,scrollbars=0,width=800,height=550');
+              } else {
+                BGcall("openTab", href);
+              }
+            });
+            return false;
+          });
+        });
