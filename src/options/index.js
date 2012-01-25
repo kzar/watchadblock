a462c5c72c338ebca7f4ae860fd6669e6deb4008
adblockforchrome@gmail.com <adblockforchrome@gmail.com>
2012-01-25 20:02:06 +0000

    Update issue 6425 Whitespace-only changes in .js files
    svn path=/branches/issue-6425/; revision=3520

--- src/options/index.js
+++ src/options/index.js
@@ -1,76 +1,76 @@
-      function load_options() {
-        // Check or uncheck each option.
-        BGcall("get_settings", function(settings) {
-          optionalSettings = settings;
-          $("#tabpages").
-            tabs({ 
-              spinner: "",
-              cache: true,
-              cookie: {},
-              load: function(event, ui) {
-                //translation
-                localizePage();
+function load_options() {
+  // Check or uncheck each option.
+  BGcall("get_settings", function(settings) {
+    optionalSettings = settings;
+    $("#tabpages").
+      tabs({ 
+        spinner: "",
+        cache: true,
+        cookie: {},
+        load: function(event, ui) {
+          //translation
+          localizePage();
 
-                $(".advanced").toggle(optionalSettings.show_advanced_options);
-                $(".chrome-only").toggle(!SAFARI);
+          $(".advanced").toggle(optionalSettings.show_advanced_options);
+          $(".chrome-only").toggle(!SAFARI);
 
-                // Must load tab .js here: CSP won't let injected html inject <script>
-                var scriptToLoad = ui.tab.dataset.script; // see index.html:data-script
-                // CSP blocks eval, which $().append(scriptTag) uses
-                var s = document.createElement("script");
-                s.src = scriptToLoad;
-                document.body.appendChild(s);
-              },
-            }).
-            show();
-        });
+          // Must load tab .js here: CSP won't let injected html inject <script>
+          var scriptToLoad = ui.tab.dataset.script; // see index.html:data-script
+          // CSP blocks eval, which $().append(scriptTag) uses
+          var s = document.createElement("script");
+          s.src = scriptToLoad;
+          document.body.appendChild(s);
+        },
+      }).
+      show();
+  });
+}
+var optionalSettings = {};
+load_options();
+
+function displayVersionNumber() {
+  try {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", chrome.extension.getURL('manifest.json'), true);
+    xhr.onreadystatechange = function() {
+      if(this.readyState == 4) {
+        var theManifest = JSON.parse(this.responseText);
+        $("#version_number").text(translate("optionsversion", [theManifest.version]));
       }
-      var optionalSettings = {};
-      load_options();
+    };
+    xhr.send();
+  } catch (ex) {} // silently fail
+}
 
-      function displayVersionNumber() {
-        try {
-          var xhr = new XMLHttpRequest();
-          xhr.open("GET", chrome.extension.getURL('manifest.json'), true);
-          xhr.onreadystatechange = function() {
-            if(this.readyState == 4) {
-              var theManifest = JSON.parse(this.responseText);
-              $("#version_number").text(translate("optionsversion", [theManifest.version]));
-            }
-          };
-          xhr.send();
-        } catch (ex) {} // silently fail
+$(function() {
+  displayVersionNumber();
+
+  if (navigator.language.substring(0, 2) != "en")
+    $("#translation_credits").text(translate("translator_credit"));
+
+  //translation
+  localizePage();
+});
+
+
+
+
+$(function() {
+  if (SAFARI && LEGACY_SAFARI) {
+    $("#safari50_updatenotice").show();
+  }
+
+  $('#paymentlink').click(function() {
+    BGcall("storage_get", "userid", function(userId) {
+      var href = "http://chromeadblock.com/pay/?source=O&u=" + userId;
+      if (SAFARI) {
+        // Safari target=_blank opens a new window by default, so we have to force its
+        // height to be correct.
+        window.open(href, "payment", 'location=0,status=0,scrollbars=0,width=800,height=550');
+      } else {
+        BGcall("openTab", href);
       }
-
-      $(function() {
-        displayVersionNumber();
-
-        if (navigator.language.substring(0, 2) != "en")
-          $("#translation_credits").text(translate("translator_credit"));
-
-        //translation
-        localizePage();
-      });
-
-
-
-
-        $(function() {
-          if (SAFARI && LEGACY_SAFARI) {
-            $("#safari50_updatenotice").show();
-          }
-
-          $('#paymentlink').click(function() {
-            BGcall("storage_get", "userid", function(userId) {
-              var href = "http://chromeadblock.com/pay/?source=O&u=" + userId;
-              if (SAFARI) {
-                // Safari target=_blank opens a new window by default, so we have to force its
-                // height to be correct.
-                window.open(href, "payment", 'location=0,status=0,scrollbars=0,width=800,height=550');
-              } else {
-                BGcall("openTab", href);
-              }
-            });
-            return false;
-          });
-        });
+    });
+    return false;
+  });
+});
--- src/pages/subscribe.js
+++ src/pages/subscribe.js
@@ -1,61 +1,60 @@
+$(function() {
+  // Translation
+  localizePage();
 
-      $(function() {
-        // Translation
-        localizePage();
+  // When the subscription is finished or aborted
+  function finished(success) {
+    $('#result').text(success ?
+      translate("subscribingfinished") :
+      translate("subscribingfailed"));
+    window.setTimeout(function() {
+      window.close();
+    }, success ? 2000 : 3500);
+  }
 
-        // When the subscription is finished or aborted
-        function finished(success) {
-          $('#result').text(success ?
-            translate("subscribingfinished") :
-            translate("subscribingfailed"));
-          window.setTimeout(function() {
-            window.close();
-          }, success ? 2000 : 3500);
-        }
+  //Get the URL
+  var queryparts = parseUri.parseSearch(document.location.search);
 
-        //Get the URL
-        var queryparts = parseUri.parseSearch(document.location.search);
+  if (!/^https?\:\/\//i.test(queryparts.location)) {
+    finished(false);
+    return; // only should run on http/s pages
+  }
 
-        if (!/^https?\:\/\//i.test(queryparts.location)) {
-          finished(false);
-          return; // only should run on http/s pages
-        }
+  //Set the link to the list.  If it's really long, make it wrap nicely.
+  $('a').
+    attr('href', queryparts.location).
+    text(queryparts.location.replace(/(.{48,64}\W)/g, '$1 '));
 
-        //Set the link to the list.  If it's really long, make it wrap nicely.
-        $('a').
-          attr('href', queryparts.location).
-          text(queryparts.location.replace(/(.{48,64}\W)/g, '$1 '));
+  //After the subscribing is finished...
+  function finished(success) {
+    $('span#result').text(success ?
+      translate("subscribingfinished") :
+      translate("subscribingfailed"));
+    window.setTimeout(function() {
+      window.close();
+    }, success ? 2000 : 3500);
+  }
 
-        //After the subscribing is finished...
-        function finished(success) {
-          $('span#result').text(success ?
-            translate("subscribingfinished") :
-            translate("subscribingfailed"));
-          window.setTimeout(function() {
-            window.close();
-          }, success ? 2000 : 3500);
-        }
+  chrome.extension.onRequest.addListener(function(request) {
+    if (request.command != "filters_updated")
+      return;
+    if ($('span#result').text())
+      return;
 
-        chrome.extension.onRequest.addListener(function(request) {
-          if (request.command != "filters_updated")
-            return;
-          if ($('span#result').text())
-            return;
+    BGcall('get_subscriptions_minus_text', function(subs) {
+      var sub = subs['url:' + queryparts.location];
+      if (!sub || sub.last_update) {
+        // It was a well known id, so assume it succeeded, or the
+        // last_update property exists, so it succeeded
+        finished(true);
+      } else if (sub.last_update_failed)
+        finished(false);
+    });
+  });
 
-          BGcall('get_subscriptions_minus_text', function(subs) {
-            var sub = subs['url:' + queryparts.location];
-            if (!sub || sub.last_update) {
-              // It was a well known id, so assume it succeeded, or the
-              // last_update property exists, so it succeeded
-              finished(true);
-            } else if (sub.last_update_failed)
-              finished(false);
-          });
-        });
-
-        //Subscribe to a list
-        var requiresList = queryparts.requiresLocation ?
-            "url:" + queryparts.requiresLocation : undefined;
-        BGcall("subscribe",
-            {id: 'url:' + queryparts.location, requires:requiresList});
-      });
+  //Subscribe to a list
+  var requiresList = queryparts.requiresLocation ?
+      "url:" + queryparts.requiresLocation : undefined;
+  BGcall("subscribe",
+      {id: 'url:' + queryparts.location, requires:requiresList});
+});
