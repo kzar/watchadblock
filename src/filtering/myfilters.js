d4e1df289c82dd25a4e05ac20e445c6054a3c3b0
Famlam <fam.lam@live.nl>
2012-03-04 18:00:27 +0000

    Update issue 6544 Owner: -fam.lam@live.nl Change the Dutch one to https. I compared all others with the ones listed on the known subscriptions page and they match.
    That leaves just
    chromeadblock.com -> Michael?
    and
    mozdev.org (2x)
    But the latter doesn't have a valid https certificate, so I'd prefer to keep it on http:// before Chrome implements some security measurement that disallows extensions to access such resources.
    
    svn path=/trunk/; revision=3651

--- src/filtering/myfilters.js
+++ src/filtering/myfilters.js
@@ -421,7 +421,7 @@ MyFilters.prototype._make_subscription_options = function() {
       requiresList: "easylist",
     },
     "dutch": { // Additional Dutch filters
-      url: "http://dutchadblockfilters.googlecode.com/svn/trunk/AdBlock_Dutch_hide.txt",
+      url: "https://dutchadblockfilters.googlecode.com/svn/trunk/AdBlock_Dutch_hide.txt",
       requiresList: "easylist",
     },
     "easylist_plus_finnish": { // Additional Finnish filters
