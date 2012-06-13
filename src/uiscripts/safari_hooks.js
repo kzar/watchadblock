9d7cbf41e6032514fe50429be529bbae01537286
adblockforchrome@gmail.com <adblockforchrome@gmail.com>
2012-06-13 18:07:26 +0000

    Update issue 6847 Reimplement emit_page_broadcast in Safari without using ports. Delete ports-related code from port.js as that was the only use anywhere in AdBlock.
    svn path=/branches/issue-6847/; revision=4014

--- src/uiscripts/safari_hooks.js
+++ src/uiscripts/safari_hooks.js
@@ -1,23 +1,20 @@
 // This file is only loaded by Safari.  Chrome uses chrome.tabs.executeScript
 // in lieu of broadcasts, and registers context menus in background.html.
 
-// TODO: free to make this Safari-specific if that's helpful
-(function() {
-  // Handle broadcasted instructions
-  var dispatcher = {};
-  dispatcher['send_content_to_back'] = send_content_to_back;
+// Handle emit_page_broadcast
+safari.self.addEventListener("message", function(event) {
+  if (event.name == "page-broadcast") {
+    // Only one command is supported
+    if (event.message.fn == 'send_content_to_back') {
+      send_content_to_back();
+    }
+  }
+});
 
-  var port = chrome.extension.connect({name: "Broadcast receiver"});
-  port.onMessage.addListener(function(request) {
-    if (dispatcher[request.fn])
-      dispatcher[request.fn](request.options);
-  });
-})();
-
+// Handle message event generated in toolbar button and right click menu item
+// "command" event handler
 if (window == window.top) {
   safari.self.addEventListener("message", function(event) {
-    // Handle message event generated in toolbar button and right click menu
-    // item "command" event handler
     if (event.name == "show-whitelist-wizard")
       top_open_whitelist_ui({});
     else if (event.name == "show-blacklist-wizard")
