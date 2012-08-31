6bda11f5ee00f0a88f4b3988e0774318b75bff88
Famlam <fam.lam@live.nl>
2012-08-31 15:37:26 +0000

    Update issue 7042 Status: NeedsReview Use the same transparent-image-URL as ABP and Ghostery so Chrome won't show a conflict warning.
    svn path=/branches/issue-7042/; revision=4299

--- src/background.js
+++ src/background.js
@@ -203,7 +203,7 @@
       log("[DEBUG]", "Block result", blocked, details.type, frameDomain, details.url.substring(0, 100));
       if (blocked && elType === ElementTypes.image) {
         // 1x1 px transparant image.
-        return {redirectUrl: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACXBIWXMAAAAnAAAAJwEqCZFPAAAAC0lEQVQIHWNgAAIAAAUAAY27m/MAAAAASUVORK5CYII="};
+        return {redirectUrl: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=="};
       }
       if (blocked && elType === ElementTypes.subdocument) {
         return { redirectUrl: "about:blank" };
