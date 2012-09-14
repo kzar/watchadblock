8710f63873d2ba1e7a4a2d22cfa5686d1173a0d8
adblockforchrome@gmail.com <adblockforchrome@gmail.com>
2012-09-14 22:20:14 +0000

    Update issue 7082   Status: Fixed   Merged into trunk
      Thanks for tracking this one down.
    
    svn path=/trunk/; revision=4368

--- src/background.js
+++ src/background.js
@@ -248,7 +248,11 @@
       var text = custom_filters[i];
       if (!Filter.isWhitelistFilter(text))
         continue;
-      var filter = PatternFilter.fromText(text);
+      try {
+        var filter = PatternFilter.fromText(text);
+      } catch (ex) { 
+        continue; 
+      }
       if (!filter.matches(url, ElementTypes.document, false))
         continue;
 
