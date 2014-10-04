db46a9b483083cd7ba879079b18ab867da241e43
Dave Barker <kzar@kzar.co.uk>
2014-10-04 14:54:30 +0100

    Release new version 2.6.35
    +2.6.35: bug fix
    +

--- src/ytchannel.js
+++ src/ytchannel.js
@@ -22,27 +22,13 @@ if (/youtube/.test(document.location.hostname)) {
   //                 - reload the page, so AdBlock can properly whitelist the channel
   //                   (if channel is whitelisted by user)
   function channel() {
-    if ((url.search("channel=") < 0) && (/channel\/|watch/.test(url)) && (url.search("feed") < 0)) {           
-      if (/channel/.test(url)) {
-        var get_yt_name = document.querySelector(".epic-nav-item-heading").innerText;
-        if (get_yt_name === null) {
-          get_yt_name = document.querySelector(".qualified-channel-title-text a[href*='/user/']");
-          var extracted_name = get_yt_name.getAttribute("href").split('/').pop();
-        } else {
-          var extracted_name = get_yt_name.replace(/\s/g, '');
-        }
-        var new_url = url+"?&channel="+extracted_name;
-      } else {
-        var get_yt_name = document.querySelector("#watch7-user-header a[href*='/user/']");
-        if (get_yt_name === null) { 
-          //in Safari 5, the anchor has a different parent tag
-          get_yt_name = document.querySelector("#ud a[href*='/user/']");
-        }
-        if (get_yt_name === null) {
-          return;
-        }
+    if ((url.search("channel=") < 0) && (/channel\/|watch/.test(url)) && (url.search("feed") < 0)) { 
+        var get_yt_name = document.querySelector("span[itemprop='author'] link");
         var extracted_name = get_yt_name.getAttribute("href").split('/').pop();
-        var new_url = url+"&channel="+extracted_name;
+      if (/channel/.test(url)) {
+          var new_url = url+"?&channel="+extracted_name;
+      } else {       
+          var new_url = url+"&channel="+extracted_name;
       }
       // Add the name of the channel to the end of URL
       window.history.replaceState(null,null,new_url);
