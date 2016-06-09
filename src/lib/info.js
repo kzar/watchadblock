3811aa372ec6f4f2d0b39448baf3de986dabea65
Dave Barker <kzar@kzar.co.uk>
2016-06-09 22:26:22 +0100

    Release new version 3.0.2
    +3.0.2:
    +- Fixed the 'bug report' link on the Support tab for non-English users.  (#105)
    +

--- src/lib/info.js
+++ src/lib/info.js
@@ -55,7 +55,7 @@
 
   require.scopes.info = {
     addonName: "adblockforchrome",
-    addonVersion: "3.0.1",
+    addonVersion: "3.0.2",
 
     application: application,
     applicationVersion: applicationVersion,
