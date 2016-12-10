42a283d1475d2cac818b57b739647f853cdaa695
Dave Barker <kzar@kzar.co.uk>
2016-12-10 02:08:11 +0000

    Release new version 3.7.0
    +3.7.0:
    +- Updated translations.
    +

--- src/lib/info.js
+++ src/lib/info.js
@@ -55,7 +55,7 @@
 
   require.scopes.info = {
     addonName: "adblockforchrome",
-    addonVersion: "3.6.0",
+    addonVersion: "3.7.0",
 
     application: application,
     applicationVersion: applicationVersion,
