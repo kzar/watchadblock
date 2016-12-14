b0c645174c307b03b716b8d950f92b810ef74691
Dave Barker <kzar@kzar.co.uk>
2016-12-14 22:28:11 +0000

    Release new version 3.8.0
    +3.8.0:
    +- Updated translations.
    +- Fixed an issue with passing of custom filters to our support site on the 'Report an Ad' page, and the 'Report a Bug' page. (# 198)
    +- Added rich notifications as a user survey type. (# 195)
    +
    +

--- src/lib/info.js
+++ src/lib/info.js
@@ -55,7 +55,7 @@
 
   require.scopes.info = {
     addonName: "adblockforchrome",
-    addonVersion: "3.7.0",
+    addonVersion: "3.8.0",
 
     application: application,
     applicationVersion: applicationVersion,
