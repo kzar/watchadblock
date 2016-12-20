b84f17e6d0c00d660bfc2fe40e62745413d0c167
Dave Barker <kzar@kzar.co.uk>
2016-12-20 21:41:34 +0000

    Release new version 3.8.2
    +3.8.2:
    +- Updated the donate to us link on the AdBlock menu.  (#204)
    +
    -- Fixed a bug on the 'Options' page, 'Filter List' tab.  All of the checkboxes would be inadvertently checked when a user clicked the 'Update Now' button, or other similar actions.  (#129)
    +- Fixed a bug on the 'Options' page, 'Filter List' tab.  All of the checkboxes would be inadvertently checked when a user clicked the 'Update Now' button, or other similar actions.  (#129)

--- src/lib/info.js
+++ src/lib/info.js
@@ -55,7 +55,7 @@
 
   require.scopes.info = {
     addonName: "adblockforchrome",
-    addonVersion: "3.8.1",
+    addonVersion: "3.8.2",
 
     application: application,
     applicationVersion: applicationVersion,
