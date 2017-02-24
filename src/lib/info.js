82635a4cac7f5f3de2b3056721da1df250980513
Dave Barker <kzar@kzar.co.uk>
2017-02-24 16:48:00 +0000

    Release new version 3.8.8
    +3.8.8:
    +- Updated translations.
    +- Updated 'bandaids' processing to help block ads on certain web sites. (#216)
    +

--- src/lib/info.js
+++ src/lib/info.js
@@ -55,7 +55,7 @@
 
   require.scopes.info = {
     addonName: "adblockforchrome",
-    addonVersion: "3.8.7",
+    addonVersion: "3.8.8",
 
     application: application,
     applicationVersion: applicationVersion,
