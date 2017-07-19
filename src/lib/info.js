675a0d626367b03705baea44149abf3a3245180d
Dave Barker <kzar@kzar.co.uk>
2017-07-19 16:25:57 +0100

    Release new version 3.14.0
    +3.14.0:
    +- Updated Translations.
    +- Updated 'bandaids' processing to help block ads on certain web sites. (#237)
    +- Added the injection of the AdBlock unique id into JavaScript memory space on getadblock.com sites. (#237)
    +

--- src/lib/info.js
+++ src/lib/info.js
@@ -55,7 +55,7 @@
 
   require.scopes.info = {
     addonName: "adblockforchrome",
-    addonVersion: "3.13.0",
+    addonVersion: "3.14.0",
 
     application: application,
     applicationVersion: applicationVersion,
