de3dc48b31e4d1ca8d2e78a377e0dec28479e6e2
Dave Barker <kzar@kzar.co.uk>
2019-08-07 20:09:15 +0100

    Release new version 3.51.1
    +Version: 3.51.1, Date: 2019-08-07
    +- Fixed a few issues in the 'MyAdBlock' image replacement functionality
    +- Fixed an issue on the AdBlock Option page not showing the translation credits widget
    +- Added a new data sync feature for MyAdBlock users
    +- Updated translations
    +

--- src/qunit/tests.js
+++ src/qunit/tests.js
@@ -1532,7 +1532,7 @@ if (!application)
 
 
 exports.addonName = "adblockforchrome";
-exports.addonVersion = "3.50.1";
+exports.addonVersion = "3.51.1";
 
 exports.application = application;
 exports.applicationVersion = applicationVersion;
